1a0471546b48bf757e239562f8e13802dbdac8e0
Bogdan Chadkin <trysound@yandex.ru>
2018-04-10 00:23:07 +0300

    Upgrade tests

--- tests/index.test.js
+++ tests/index.test.js
@@ -27,7 +27,7 @@ test("fail on invalid options", () => {
     sizeSnapshot({
       minify: true,
       snapshot: "",
-      treeshake: true
+      updateSnapshot: true
     });
   }).toThrowError(/Options "minify", "snapshot" are invalid/);
 });
@@ -37,16 +37,16 @@ test("write bundled, minified and gzipped size of es bundle", async () => {
   await runRollup({
     input: "fixtures/redux.js",
     plugins: [sizeSnapshot({ snapshotPath, printInfo: false })],
-    output: { file: "output.js", format: "es" }
+    output: { file: "output.js", format: "cjs" }
   });
   const snapshot = pullSnapshot(snapshotPath);
 
   expect(snapshot).toEqual(
     expect.objectContaining({
       "output.js": {
-        bundled: 10971,
-        minified: 5293,
-        gzipped: 2032
+        bundled: 11160,
+        minified: 5464,
+        gzipped: 2091
       }
     })
   );
@@ -57,7 +57,7 @@ test("print sizes", async () => {
   const snapshotPath = "fixtures/print.size-snapshot.json";
   const snapshot = await runRollup({
     input: "fixtures/redux.js",
-    output: { file: "output.js", format: "es" },
+    output: { file: "output.js", format: "cjs" },
     plugins: [sizeSnapshot({ snapshotPath })]
   });
 
@@ -65,35 +65,10 @@ test("print sizes", async () => {
 
   expect(last(consoleInfo.mock.calls).map(stripAnsi)).toEqual([
     expect.stringContaining(
-      'Computed sizes of "output.js" with "es" format\n' +
-        "  bundled: 10,971 B\n" +
-        "  minified with uglify: 5,293 B\n" +
-        "  minified and gzipped: 2,032 B\n"
-    )
-  ]);
-
-  consoleInfo.mockRestore();
-});
-
-test("print sizes with treeshaked size", async () => {
-  const consoleInfo = jest.spyOn(console, "info").mockImplementation(() => {});
-  const snapshotPath = "fixtures/print-with-treeshaking.size-snapshot.json";
-  const snapshot = await runRollup({
-    input: "fixtures/redux.js",
-    output: { file: "output.js", format: "es" },
-    plugins: [sizeSnapshot({ snapshotPath, treeshake: true })]
-  });
-
-  pullSnapshot(snapshotPath);
-
-  expect(last(consoleInfo.mock.calls).map(stripAnsi)).toEqual([
-    expect.stringContaining(
-      'Computed sizes of "output.js" with "es" format\n' +
-        "  bundled: 10,971 B\n" +
-        "  minified with uglify: 5,293 B\n" +
-        "  minified and gzipped: 2,032 B\n" +
-        "  treeshaked with rollup and uglified: 0 B\n" +
-        "  treeshaked with webpack and uglified: 566 B\n"
+      'Computed sizes of "output.js" with "cjs" format\n' +
+        "  bundled: 11,160 B\n" +
+        "  minified with uglify: 5,464 B\n" +
+        "  minified and gzipped: 2,091 B\n"
     )
   ]);
 
@@ -104,16 +79,16 @@ test("not affected by following uglify plugin", async () => {
   const snapshotPath = "fixtures/uglify.size-snapshot.json";
   await runRollup({
     input: "fixtures/redux.js",
-    output: { file: "output.js", format: "es" },
+    output: { file: "output.js", format: "cjs" },
     plugins: [sizeSnapshot({ snapshotPath, printInfo: false }), uglify()]
   });
 
   expect(pullSnapshot(snapshotPath)).toEqual(
     expect.objectContaining({
       "output.js": {
-        bundled: 10971,
-        minified: 5293,
-        gzipped: 2032
+        bundled: 11160,
+        minified: 5464,
+        gzipped: 2091
       }
     })
   );
@@ -165,12 +140,37 @@ test("fail with update false if bundled, minified or gziped sizes are not matche
   consoleError.mockRestore();
 });
 
-test("write treeshaked with rollup and webpack sizes", async () => {
+test("print sizes with treeshaked size for 'es' format", async () => {
+  const consoleInfo = jest.spyOn(console, "info").mockImplementation(() => {});
+  const snapshotPath = "fixtures/print-with-treeshaking.size-snapshot.json";
+  const snapshot = await runRollup({
+    input: "fixtures/redux.js",
+    output: { file: "output.js", format: "es" },
+    plugins: [sizeSnapshot({ snapshotPath })]
+  });
+
+  pullSnapshot(snapshotPath);
+
+  expect(last(consoleInfo.mock.calls).map(stripAnsi)).toEqual([
+    expect.stringContaining(
+      'Computed sizes of "output.js" with "es" format\n' +
+        "  bundled: 10,971 B\n" +
+        "  minified with uglify: 5,293 B\n" +
+        "  minified and gzipped: 2,032 B\n" +
+        "  treeshaked with rollup and uglified: 0 B\n" +
+        "  treeshaked with webpack and uglified: 566 B\n"
+    )
+  ]);
+
+  consoleInfo.mockRestore();
+});
+
+test("write treeshaked with rollup and webpack sizes for 'es' format", async () => {
   const snapshotPath = "fixtures/rollupTreeshake.size-snapshot.json";
   const snapshot = await runRollup({
     input: "fixtures/redux.js",
     output: { file: "output.js", format: "es" },
-    plugins: [sizeSnapshot({ snapshotPath, treeshake: true, printInfo: false })]
+    plugins: [sizeSnapshot({ snapshotPath, printInfo: false })]
   });
 
   expect(pullSnapshot(snapshotPath)).toEqual(
@@ -185,12 +185,12 @@ test("write treeshaked with rollup and webpack sizes", async () => {
   );
 });
 
-test("treeshake pure annotations with rollup, webpack and uglify", async () => {
+test("treeshake pure annotations with rollup and uglify or webpack", async () => {
   const snapshotPath = "fixtures/pure-annotated.size-snapshot.json";
   await runRollup({
     input: "fixtures/pure-annotated.js",
     output: { file: "output.js", format: "es" },
-    plugins: [sizeSnapshot({ snapshotPath, treeshake: true, printInfo: false })]
+    plugins: [sizeSnapshot({ snapshotPath, printInfo: false })]
   });
 
   expect(pullSnapshot(snapshotPath)).toEqual(
@@ -205,13 +205,13 @@ test("treeshake pure annotations with rollup, webpack and uglify", async () => {
   );
 });
 
-test("treeshake with rollup or webpack and external modules", async () => {
+test("treeshake with both rollup or webpack and external modules", async () => {
   const snapshotPath = "fixtures/externals.size-snapshot.json";
   await runRollup({
     input: "fixtures/externals.js",
     external: ["react"],
     output: { file: "output.js", format: "es" },
-    plugins: [sizeSnapshot({ snapshotPath, treeshake: true, printInfo: false })]
+    plugins: [sizeSnapshot({ snapshotPath, printInfo: false })]
   });
 
   expect(pullSnapshot(snapshotPath)).toEqual(
