da1d0bd9f04972a6b764e61a7ef30d323eb886e3
Christopher J. Brody <chris.brody+brodybits@gmail.com>
2021-12-07 11:09:19 -0500

    fix(deps): update acorn -> 8
    update API call as needed by acorn@8

--- src/treeshakeWithRollup.js
+++ src/treeshakeWithRollup.js
@@ -53,7 +53,10 @@ export const treeshakeWithRollup = (code: string): Promise<Output> => {
       })
     )
     .then((result): Output => {
-      const ast = parse(result.code, { sourceType: "module" });
+      const ast = parse(result.code, {
+        sourceType: "module",
+        ecmaVersion: 2020,
+      });
       const import_statements = ast.body
         // collect all toplevel import statements
         .filter((node) => node.type === "ImportDeclaration")
